b6174bfb9cad50e4f6326b8536ac83f5f5e6da98
Karen Sawrey <krnswry@gmail.com>
2025-02-27 15:54:14 +0100

    Create hello-bowrld.sh
    See if a build gets triggered

---
+++ hello-bowrld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# A dog-themed Hello World script
+
+echo "   /^ ^\ "
+echo "  / 0 0 \ "
+echo "  V\ Y /V "
+echo "   / - \  "
+echo "  /    |  "
+echo " V__) || "
+echo ""
+echo " 🐾  Woof! Hello World! 🐾 "
