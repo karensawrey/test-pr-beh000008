17201640f86411add845a25e7f0274708d9753cc
Karen Sawrey <krnswry@gmail.com>
2025-02-27 15:49:28 +0100

    Create hello-meowrld.sh
    Hello meowrld PR 01.

---
+++ hello-meowrld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# A cat-themed Hello World script
+
+echo "   /\_/\  "
+echo "  ( o.o ) "
+echo "   > ^ <  "
+echo ""
+echo " 🐾  Meow! Hello World! 🐾 "
+echo ""
+echo "  /\_/\  zzZ"
+echo " ( -.- )   "
+echo "  > - <    "
